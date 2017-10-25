410b326caa3a7ab5da3cc535c8544ef51a3e4f27
ppfz <ppfzppfz@gmail.com>
2017-10-24 22:05:17 -0700

    Update urls.py

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from firstapp.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/$',hello),
-    url(r'^hello/(\d+)/$',hello1),
 ]
+
+# Use include() to add URLS from the web application 
+from django.conf.urls import include
+
+urlpatterns += [
+	url(r'^web/', include('web.urls')),
+]
+
+#Add URL maps to redirect the base URL to our application
+from django.views.generic import RedirectView
+urlpatterns += [
+	url(r'^$', RedirectView.as_view(url='/web/', permanent=True)),
+]
+
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
