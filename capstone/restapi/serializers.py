3be65f943b8adf9700faf933128ab969ddecd9c1
Ubuntu <ubuntu@ip-172-31-25-19.us-west-2.compute.internal>
2017-10-30 21:37:23 +0000

    decimal problem not soloved

--- capstone/restapi/serializers.py
+++ capstone/restapi/serializers.py
@@ -2,30 +2,25 @@ from restapi.models import *
 from web.models import *
 from rest_framework import serializers
 
+class MenuItemSerializer(serializers.Serializer):
+    '''
+        class Meta:
 
+                model=Product
+                fields = ('Product_id','Product_name','Price','Description')
+    '''
 
-class MenuItemSerializer(serializers.ModelSerializer):
-	class Meta:
-#		model=Menu_Item
-#		fields = ('Menu_id','Menu_id_id','Product_id','Product_id_id')
-		model=Product
-		fields = ('Product_id','Product_name','Price','Description')
+    Product_id = serializers.CharField(max_length=200)
+    Product_name = serializers.CharField(max_length=200)
+    Price = serializers.DecimalField(decimal_places=2, max_digits=10,coerce_to_string=False)
+    Description = serializers.CharField(max_length=500)
 
+#    def create(self, validated_data):
+#	return Product.objects.create(**validated_data)
+	
 
 
 class MenuSerializer(serializers.ModelSerializer):
 	class Meta:
 		model=Menu
 		fields = ('Type','Menu_id','Res_id','Res_id_id')
-
-#	Type= serializers.CharField(max_length=100)
-#	Menu_id= serializers.CharField(max_length=100)
-	
-#	def restore_object(self, attrs, instance=None):
-#		if instance:
-#			instance.Type=attrs['Type']
-#			instance.Menu_id=attrs['Menu_id']
-	#		instance.res_name= attrs['res_name']
-#			instance.author= attrs[]
-#			return instance
-	#	return Menu(**attrs)
