b3f1ad22dd9c74b183bae4337a8ed8f649b2e9a1
ppfz <ppfzppfz@gmail.com>
2017-11-06 12:26:45 -0800

    Update urls.py

--- capstone/web/urls.py
+++ capstone/web/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
 	url(r'^order/$', views.OrderDetail.as_view(), name='order'),
+	# redirect after updating status - Xie 11/6
+	url(r'^(?P<pk>\d+)/$', views.statusUpdate, name='status'),
 ]
