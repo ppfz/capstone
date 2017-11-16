fca0776c01b580d54338f64cc890cd86221617f5
ppfz <ppfzppfz@gmail.com>
2017-11-16 14:54:22 -0800

    Update admin.py

--- capstone/restapi/admin.py
+++ capstone/restapi/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from markdownx.admin import MarkdownxModelAdmin
 from .models import MyModel
 
-
 admin.site.register(MyModel, MarkdownxModelAdmin)
 
 
-#from  web.models import *
-
-#admin.site.register(Menu, MarkdownxModelAdmin)
