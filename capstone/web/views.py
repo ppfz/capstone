7fb0be98b2a22844b7ca9dd8a4e3d7ea5fd37ae7
ppfz <ppfzppfz@gmail.com>
2017-11-20 15:41:30 -0800

    Update views.py

--- capstone/web/views.py
+++ capstone/web/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-# Create your views here.
-
-from .models import Beacon, Menu, Menu_Item, Order, Order_Item, Payment, Restaurant, Product
 from django.views import generic
+
+# Import models
+from .models import Beacon, Menu, Menu_Item, Order, Order_Item, Payment, Restaurant, Product
+
 
 # Create index page
 def index(request):
@@ -21,17 +21,31 @@ def index(request):
 		context={'num_restaurants':num_restaurants},
     )
 
-# Create order page (Updated by Xie 11/6)
-class OrderDetail(generic.ListView):
+
+# Create order page	
+class OrderDetail(LoginRequiredMixin,generic.ListView):
 	#model = Order_Item
 	paginate_by = 6
 	context_object_name = 'Order_Details'   # your own name for the list as a template variable
 	def get_queryset(self):
 		return Order_Item.objects.select_related('Order_id').filter(Order_id__Res_id=self.request.user.username).filter(Status='CK')
+	#queryset = Order_Item.objects.select_related('Order_id').fliter(Order_id.res_id=self.request.user)
 	template_name = 'web/order_list.html'
+	#def userstr(self):
+		#return self.request.user.username		
+		
 
-# Updated the status of order_item in database and remove it from the screen - Xie 11/6
+# Update the delivery status
 def statusUpdate(request, pk):
 	if pk:
 		Order_Item.objects.filter(pk=pk).update(Status='Delivered')
 		return HttpResponseRedirect('/web/order')
+
+		
+# Create Payment Page		
+class PaymentDetail(LoginRequiredMixin,generic.ListView):
+	paginate_by = 6
+	context_object_name = 'Payment_Details'   
+	def get_queryset(self):
+		return Payment.objects.select_related('Order_id').filter(Order_id__Res_id=self.request.user.username)
+	template_name = 'web/payment_list.html'
